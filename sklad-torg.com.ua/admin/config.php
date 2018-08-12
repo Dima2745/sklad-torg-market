df63e61d055b13397b8e2284b33bffee12fa836d
Dima Kruhlyi <dimakruhly@gmail.com>
2018-08-12 08:58:27 +0300

    Add files via upload

---
+++ sklad-torg.com.ua/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.sklad-torg.com.ua/admin/');
+define('HTTP_CATALOG', 'http://www.sklad-torg.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.sklad-torg.com.ua/admin/');
+define('HTTPS_CATALOG', 'http://www.sklad-torg.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/admin/');
+define('DIR_SYSTEM', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/system/');
+define('DIR_DATABASE', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/system/database/');
+define('DIR_LANGUAGE', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/admin/language/');
+define('DIR_TEMPLATE', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/admin/view/template/');
+define('DIR_CONFIG', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/system/config/');
+define('DIR_IMAGE', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/image/');
+define('DIR_CACHE', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/system/cache/');
+define('DIR_DOWNLOAD', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/download/');
+define('DIR_LOGS', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/system/logs/');
+define('DIR_CATALOG', '/sata2/home/users/proektik/www/www.sklad-torg.com.ua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqliz');
+define('DB_HOSTNAME', 'db16.freehost.com.ua');
+define('DB_USERNAME', 'proektik_skprod');
+define('DB_PASSWORD', 'bos23E77l');
+define('DB_DATABASE', 'proektik_skprod');
+define('DB_PREFIX', 'oc_');
+?>
