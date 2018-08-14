b7b4421f5cf92fa69f52db7912ab37e5b57b8fb6
Dima Kruhlyi <dimakruhly@gmail.com>
2018-08-14 08:28:30 +0300

    Add files via upload

---
+++ sklad-torg.com.ua/catalog/language/english/module/kuler_filter.php
@@ -0,0 +1,29 @@
+<?php
+$_['heading_title']                             = 'Фильтр';
+
+$_['heading_title_result']                      = 'Результаты';
+$_['text_all']                                  = '-- Все --';
+
+$_['text_clear']                                = 'Очистить';
+$_['text_filter']                               = 'Фильтр';
+
+$_['text_filter_by_category']                   = 'Фильтр по категории';
+$_['text_current_category_filter']              = 'Выбранный фильтр';
+$_['text_select_additional_category_filter']    = 'Выбрать дополнительный фильтр';
+
+$_['text_filter_by_manufacturer']               = 'Фильтр по производителям';
+$_['text_current_manufacturer_filter']          = 'Выбранный фильтр по производителям';
+$_['text_select_additional_manufacturer_filter']= 'Выбрать фильтр по производителям';
+
+$_['text_filter_by_attributes']                 = 'Фильтр по атрибутам';
+$_['text_current_attribute_filter']             = 'Выбранный фильтр';
+$_['text_select_additional_attribute_filter']   = 'Выбрать дополнительный фильтр по опциям';
+
+$_['text_filter_by_options']                    = 'Фильтр по опциям';
+$_['text_current_option_filter']                = 'Выбранный фильтр по опциям';
+$_['text_select_additional_option_filter']      = 'Выбрать дополнительный фильтр по опциям';
+
+$_['text_search']                               = 'Поиск';
+$_['text_price']                                = 'Цена';
+
+$_['text_empty']                                = 'Нет товаров по данному фильтру.';
